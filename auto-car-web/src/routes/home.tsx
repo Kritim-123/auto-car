60c14a0f33027eac18f92685dcf7760386fbde42
Kritim-123 <kritimbastola2005@gmail.com>
2025-03-15 12:17:33 -0600

    Added expandable card for reference paper

--- auto-car-web/src/routes/home.tsx
+++ auto-car-web/src/routes/home.tsx
@@ -1,23 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
+
 export default function Home() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <div>
       <div>
-        {/* TODO : Add crousel (box) to show the papers in different box and when I click on them 
-         make them pop-up and show description. */}
-
-        <h2 className="text-2x1 font-bold my-4">References Paper</h2>
-        <ul>
-          <li>
-            A Bird's Eye View Object Detection Network for LiDAR Point Cloud.
-          </li>
-          <p>
-            The paper talks about: A novel single-stage BEV-based LiDAR object
-            detection network (BEV-Net). Efficient use of ResNet backbones for
-            flexible trade-offs between speed and accuracy. Extensive evaluation
-            on KITTI, nuScenes, and Waymo datasets. Outperforms existing
-            BEV-based methods while maintaining real-time inference speed.
-          </p>
-        </ul>
+        <h2 className="text-2x1 font-bold my-4 mx-4">References Papers</h2>
+        <Card
+          className="w-[350px] mx-4 my-4 h-auto cursor-pointer"
+          onClick={() => setExpanded(!expanded)}
+        >
+          <CardHeader>
+            <CardTitle>
+              A Bird's Eye View Object Detection Network for LiDAR Point Cloud.
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className={expanded ? "" : "line-clamp-4"}>
+              The paper talks about: A novel single-stage BEV-based LiDAR object
+              detection network (BEV-Net). Efficient use of ResNet backbones for
+              flexible trade-offs between speed and accuracy. Extensive
+              evaluation on KITTI, nuScenes, and Waymo datasets. Outperforms
+              existing BEV-based methods while maintaining real-time inference
+              speed.
+            </p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
