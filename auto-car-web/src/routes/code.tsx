197f8e67ae0f2a21cf6e5536f09b3c81146b05ce
Kritim-123 <kritimbastola2005@gmail.com>
2025-03-14 18:29:03 -0600

    some fixes

--- auto-car-web/src/routes/code.tsx
+++ auto-car-web/src/routes/code.tsx
@@ -1,10 +1,3 @@
-
-import React from 'react'
-
 export default function Code() {
-  return (
-    <div>
-      This is code component
-    </div>
-  )
+  return <div>This is code component</div>;
 }
--- auto-car-web/src/routes/home.tsx
+++ auto-car-web/src/routes/home.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-
 export default function Home() {
   return (
     <div>
-      
-      <div >
+      <div>
         {/* TODO : Add crousel (box) to show the papers in different box and when I click on them 
          make them pop-up and show description. */}
 
-        <h2 className="text-2x1 font-bold my-4">References to the Paper I read</h2>
+        <h2 className="text-2x1 font-bold my-4">
+          References to the Paper I read
+        </h2>
         <ul>
-          <li>A Bird's Eye View Object Detection Network for LiDAR Point Cloud.</li>
+          <li>
+            A Bird's Eye View Object Detection Network for LiDAR Point Cloud.
+          </li>
           <p>Description of the Paper mentioned above</p>
         </ul>
       </div>
